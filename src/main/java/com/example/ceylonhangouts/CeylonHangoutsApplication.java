51ff53763c1e69c93f02b14f4bb3fecedcb9408e
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-08-11 12:00:00 +0530

    l5

--- src/main/java/com/example/ceylonhangouts/CeylonHangoutsApplication.java
+++ src/main/java/com/example/ceylonhangouts/CeylonHangoutsApplication.java
@@ -4,8 +4,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class CeylonHangoutsApplication {
@@ -14,6 +17,15 @@ public class CeylonHangoutsApplication {
         SpringApplication.run(CeylonHangoutsApplication.class, args);
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+
+                .csrf(CsrfConfigurer::disable); // ✅ New way to disable CSRF in Spring Security 6.1+
+
+        return http.build();
+    }
+    
     @Bean
     ModelMapper modelMapper() {
         return new ModelMapper();
