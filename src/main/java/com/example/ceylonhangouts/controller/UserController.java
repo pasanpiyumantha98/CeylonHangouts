648e9af923692159a4a950f869879cb106ef2c85
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-08-11 12:00:00 +0530

    l3

--- src/main/java/com/example/ceylonhangouts/controller/UserController.java
+++ src/main/java/com/example/ceylonhangouts/controller/UserController.java
@@ -1,4 +1,11 @@
 package com.example.ceylonhangouts.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
 public class UserController {
+
+
 }
