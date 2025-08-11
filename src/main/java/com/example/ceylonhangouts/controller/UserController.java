322a002e40316e98f5825b03ee181b2a1462f12f
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-08-11 12:00:00 +0530

    l6

--- src/main/java/com/example/ceylonhangouts/controller/UserController.java
+++ src/main/java/com/example/ceylonhangouts/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.ceylonhangouts.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping("api/user")
 public class UserController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 
 
 }
