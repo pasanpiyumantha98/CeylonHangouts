38b34022dad4ecd74ee8dff1383428dfad323256
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-08-12 12:00:00 +0530

    l1

--- src/main/java/com/example/ceylonhangouts/CeylonHangoutsApplication.java
+++ src/main/java/com/example/ceylonhangouts/CeylonHangoutsApplication.java
@@ -1,7 +1,11 @@
 package com.example.ceylonhangouts;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class CeylonHangoutsApplication {
@@ -9,5 +13,16 @@ public class CeylonHangoutsApplication {
     public static void main(String[] args) {
         SpringApplication.run(CeylonHangoutsApplication.class, args);
     }
+
+    @Bean
+    ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
