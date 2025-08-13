5d1898909c2e8b8ef964c83ef42454969d98a978
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-08-13 17:30:45 +0530

    intial commit

---
+++ src/test/java/com/example/ceylonhangouts/CeylonHangoutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ceylonhangouts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeylonHangoutsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
